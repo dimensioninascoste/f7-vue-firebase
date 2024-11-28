f9c88e22ea1a08a79841861ec2143545af7215b9
mysteryfy <dimensioninascoste@gmail.com>
2024-11-28 13:12:51 +0100

    first commit

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAaO97TAGstKPBjjTcdnHdt2pjnTkd5KNk",
+  authDomain: "gta-befair.firebaseapp.com",
+  databaseURL: "https://gta-befair.firebaseio.com",
+  projectId: "gta-befair",
+  storageBucket: "gta-befair.firebasestorage.app",
+  messagingSenderId: "968074210001",
+  appId: "1:968074210001:web:1e41d4822877bc679a33f0",
+  measurementId: "G-TGE2FY4VYH"
+});
+
+const auth = getAuth(firebaseApp)
+
+onAuthStateChanged(auth, user => {
+    if(user != null) {
+        console.log('logged in');
+    } else {
+        console.log('No user');
+    }
+})
---
+++ src/pages/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar large :sliding="false">
+      <f7-nav-left>
+        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title sliding>Firebase Auth</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-ios="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
+      </f7-nav-right>
+      <f7-nav-title-large>Firebase Auth</f7-nav-title-large>
+    </f7-navbar>
+
+    <!-- Page content-->
+    <f7-block>
+      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
+    </f7-block>
+    <f7-button @click="loginWithGoogle" fill>Login With Google</f7-button>
+
+  </f7-page>
+</template>
+
+<script setup>
+import firebase from '';
+
+</script>
