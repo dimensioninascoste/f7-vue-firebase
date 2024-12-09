684ee9fe358c7b6b5bd74247ad04a246df1e2e86
Dimensioni Nascoste <80706109+dimensioninascoste@users.noreply.github.com>
2024-12-09 07:18:38 +0100

    Update firebaseConfig.js

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAaO97TAGstKPBjjTcdnHdt2pjnTkd5KNk",
-  authDomain: "gta-befair.firebaseapp.com",
-  databaseURL: "https://gta-befair.firebaseio.com",
-  projectId: "gta-befair",
-  storageBucket: "gta-befair.firebasestorage.app",
-  messagingSenderId: "968074210001",
-  appId: "1:968074210001:web:1e41d4822877bc679a33f0",
-  measurementId: "G-TGE2FY4VYH"
+-  da inserire in locale -
 });
 
 const auth = getAuth(firebaseApp)
